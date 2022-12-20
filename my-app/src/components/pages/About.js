357bc05f64c4ebe45d488713b0e103ef49112353
dy2395 <71238779+dy2395@users.noreply.github.com>
2022-12-20 01:11:34 -0500

    second draft complete

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -9,8 +9,8 @@ const About = () => {
       </div>
       <div class="container-fluid p-5">
       <p>
-       Denise (Danqi Yin) is a full-stack web developer and also a "software" engineer. She is very into to making this world a better place that she forgets about herself sometimes.
-       She used to study environmental science and engineering in the hope of improving the environment. She then worked in the solar industry as a project engineer with a thought to promote renewable energy with sustainble development for the good of people. Now she wants to build her dream on the Internet for the future of both human and nature.
+       Denise (Danqi Yin) is a full-stack web developer and also a "soft-ware" engineer. She is very into to making this world a better place that she forgets about herself sometimes :P
+       She used to study environmental science and engineering with a goal to protect the environment. She then works as a solar project engineer who aims to promote renewable energy along with sustainble development for the good of people. Now she wants to help build a brighter future for both human and nature on the powerful Internet.
       </p>
       </div>
       
